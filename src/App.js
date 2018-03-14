a2f6730abc2f605c17e2982f6bd860b99f3bf386
choumasamori <5911378@gmail.com>
2018-03-14 15:16:34 +0700

    asd

--- src/App.js
+++ src/App.js
@@ -18,29 +18,19 @@ class App extends Component {
 }
 const Home = ()=>{
   return(
-        <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome</h1>
-        </header>
+
         <p className="App-intro">
           LOGIN PAGE
         </p>
-      </div>
+
   );
       
 }
 const Main = () => {
   return(
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome</h1>
-        </header>
         <div>
           <MainPage />
         </div>
-      </div>
   );
       
 }
