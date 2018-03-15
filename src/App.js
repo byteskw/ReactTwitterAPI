98db52392236787fb64a16a7106d5be2014e63a2
choumasamori <5911378@gmail.com>
2018-03-15 13:19:56 +0700

    asd

--- src/App.js
+++ src/App.js
@@ -1,21 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-=======
 
 /* Router */
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
@@ -35,42 +18,21 @@ export class App extends Component {
           <Route exact path='/main' component={MainPage}/>
         </Switch>
       </BrowserRouter>
->>>>>>> a98cc7a37f4f793e8c74901d1bdc4db27134ed94
     );
   }
 }
 
-<<<<<<< HEAD
-export default App;
-=======
 const LoginPage = ()=>{
 
   return(
-<<<<<<< HEAD
-
-        <p className="App-intro">
-          LOGIN PAGE
-        </p>
-
-  );
-      
-=======
       <Login />
   ); 
->>>>>>> 4d5d4f825458d1a31f0677618205bd759befa9d6
 }
 
 const MainPage = () => {
   return(
-<<<<<<< HEAD
-        <div>
-          <MainPage />
-        </div>
-=======
       <Main />
->>>>>>> 4d5d4f825458d1a31f0677618205bd759befa9d6
   );
 }
 
 export default App;
->>>>>>> a98cc7a37f4f793e8c74901d1bdc4db27134ed94
