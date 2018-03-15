813c465d3fe6aa8630fb80fb11813b6c9728b41d
choumasamori <5911378@gmail.com>
2018-03-15 13:30:07 +0700

    asd

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,29 @@
 import React from 'react';
-/*import {List, ListItem} from 'material-ui/List';*/
+import TextField from 'material-ui/TextField';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export class Main extends React.Component{
     render(){
         return(
-            <div>
-                <p>
-                    MAIN PAGE
-                </p>
-            </div>
+            <MuiThemeProvider>
+                <Card>
+                  <CardHeader title="InsertNameHere" subtitle="InsertSubTitleHere" avatar="" />
+                  <CardTitle title="Post your tweet !"/>
+                    <TextField hintText="What's Happening ?" multiLine={true} rows={1} rowsMax={10} />
+                    <CardActions>
+                  <RaisedButton type="submit" label="Tweet" primary={true} />
+                   </CardActions>
+                 </Card>
+            </MuiThemeProvider>
         );
     }
+}
+const styles={
+    inputWrapper: {
+        background: 'red',
+    },
 }
 
 export default Main;
