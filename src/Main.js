0eb67a20eba9c4a1db260357a95f89362e80c5c4
choumasamori <5911378@gmail.com>
2018-03-15 14:43:51 +0700

    asd

--- src/Main.js
+++ src/Main.js
@@ -4,11 +4,26 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
 import './Main.css';
+import {List, ListItem} from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import AppBar from 'material-ui/AppBar';
 
 export class Main extends React.Component{
     render(){
         return(
             <MuiThemeProvider style={{boxShadow: 'none'}}>
+            <AppBar
+                title="insertTitleHere"
+              />
+                <div className="menu">
+            <List>
+                <ListItem primaryText="Inbox"/>
+                <ListItem primaryText="Starred"/>
+               <ListItem primaryText="Sent mail"/>
+                <ListItem primaryText="Drafts" />
+                  <ListItem primaryText="Inbox"/>
+            </List>
+            </div>
                 <div className="tweetWrapper">
                 <Card containerStyle={{background: 'rgb(23, 125, 209)', color: 'white'}}>
                   <CardHeader title="InsertNameHere" subtitle="InsertSubTitleHere" avatar="" />
@@ -21,6 +36,7 @@ export class Main extends React.Component{
                    </div>
                  </Card>
                  </div>
+                 <Divider />
                  <div className="tweetWrapper">
                  <Card>
                   <CardHeader title="InsertNameHere" subtitle="InsertSubTitleHere" avatar="" />
